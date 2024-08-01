f209dbe9c7393d421a3ee461051dbf89bc508c3a
DENNYYESSAR <dennisokeri5@gmail.com>
2024-08-02 01:01:39 +0300

    Type Checking

--- 0x00-python_variable_annotations/102-type_checking.py
+++ 0x00-python_variable_annotations/102-type_checking.py
@@ -1,34 +1,23 @@
 #!/usr/bin/env python3
-"""
-This module provides a function to zoom into an array by repeating its
-elements.
-"""
+''' Description: Use mypy to validate the following piece of code
+                 and apply any necessary changes.
+    Parameters: lst: Tuple, factor: int = 2
+'''
 
-from typing import Tuple, List
+from typing import Union, Any, Mapping, Tuple, List
 
 
-def zoom_array(lst: Tuple[int, ...], factor: int = 2) -> Tuple[int, ...]:
-    """
-    Zoom into an array by repeating each element a specified number of times.
-
-    Args:
-        lst (Tuple[int, ...]): The input tuple of integers.
-        factor (int): The number of times to repeat each element. Defaults to
-        2.
-i
-    Returns:
-        Tuple[int, ...]: A new tuple with elements repeated.
-    """
-    zoomed_in: List[int] = [
+def zoom_array(lst: Tuple, factor: int = 2) -> List:
+    ''' Variable Annotation for list '''
+    zoomed_in: List = [
         item for item in lst
         for i in range(factor)
     ]
-    return tuple(zoomed_in)
+    return zoomed_in
 
 
-array = (12, 72, 91)  # Changed from list to tuple to match the type hint
+array = tuple([12, 72, 91])
 
 zoom_2x = zoom_array(array)
 
-# Corrected the factor to be an integer
-zoom_3x = zoom_array(array, 3)
+zoom_3x = zoom_array(array, int(3.0))
