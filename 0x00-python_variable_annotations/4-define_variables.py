b87e4d131de3cf7e5f0e2d3232e1fa5a14a75952
DENNYYESSAR <dennisokeri5@gmail.com>
2024-08-01 21:45:03 +0300

    Define variables

---
+++ 0x00-python_variable_annotations/4-define_variables.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+"""
+Module defining and annotating variables with specific values.
+
+This module includes variables of various types, annotated and
+assigned with given values.
+"""
+
+a: int = 1
+"""An integer with a value of 1."""
+
+pi: float = 3.14
+"""A float with a value of 3.14."""
+
+i_understand_annotations: bool = True
+"""A boolean indicating understanding of annotations, set to True."""
+
+school: str = "Holberton"
+"""A string with the value 'Holberton'."""
